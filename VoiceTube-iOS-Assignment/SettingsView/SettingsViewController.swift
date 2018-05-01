6939983a7c42ae08c6ae582b6a32ca760de0d7b3
廖慶麟 <hohey670@gmail.com>
2018-05-01 14:00:40 +0800

    handle delegate of switchActionCell

--- VoiceTube-iOS-Assignment/SettingsView/SettingsViewController.swift
+++ VoiceTube-iOS-Assignment/SettingsView/SettingsViewController.swift
@@ -75,6 +75,7 @@ extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(of: tableContent[indexPath.row].type, for: indexPath)!
         (cell as? SwitchActionCell)?.set(title: tableContent[indexPath.row].title, isOn: true)
+        (cell as? SwitchActionCell)?.delegate = self
         (cell as? TapActionCell)?.set(title: tableContent[indexPath.row].title, detail: "10:00 AM")
         
         return cell
@@ -82,6 +83,19 @@ extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         (tableView.cellForRow(at: indexPath) as? TapActionCell)?.becomeFirstResponder()
+    }
+}
+
+extension SettingsViewController: SwitchActionCellDelegate {
+    func switchActionCellSwitch(cell: SwitchActionCell, isOn: Bool) {
+        guard let title = cell.titleLabel.text else { return }
+        switch title {
+        case "自動播放": Defaults[.autoPlay] = isOn
+        case "字幕同步": Defaults[.subtitleEnabled] = isOn
+        case "查詢單字時暫停播放": Defaults[.stopWhileQuerying] = isOn
+        case "推薦影片提醒": Defaults[.recommendationNotifiable] = isOn
+        default: break
+        }
     }
 }
 
