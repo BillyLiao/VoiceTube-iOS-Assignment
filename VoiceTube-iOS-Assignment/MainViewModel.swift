03de6b71986b93c53c9e1ccaf753f85034209844
廖慶麟 <hohey670@gmail.com>
2018-05-01 17:37:40 +0800

    load data from db while no network

--- VoiceTube-iOS-Assignment/MainViewModel.swift
+++ VoiceTube-iOS-Assignment/MainViewModel.swift
@@ -12,10 +12,17 @@ import RxCocoa
 
 public class MainViewModel {
     let items: Variable<[Item]> = Variable([])
+    var realmManager: RealmManager<ItemRealmObject> = RealmManager()
 
     public func loadData() {
-        GetItemList().perform().then(execute: { [weak self] list in
+        GetItemList().perform().then(execute: { [weak self] list -> Void in
+            list.items.forEach{ ItemRealmObject($0).save(nil) }
             self?.items.value.append(contentsOf: list.items)
-        })
+            return
+        }).catch { [weak self] (e) in
+            if let _ = self, let items = try? self!.realmManager.query().toItems() {
+                self!.items.value.append(contentsOf: items)
+            }
+        }
     }
 }
